4b74146c7312cf7cf2171d0f3e62ed91b53e4ff2
John Shellabarger <jwshe1@MacBook-Air.local>
2021-10-11 14:51:30 -0600

    responsive to contact page

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -30,41 +30,11 @@ const Contact = () => {
 
         <div className='right'>
          <div className='imgContainer'>
-         <img className='personal-img' src='https://lh3.googleusercontent.com/KYye4SteAo-5Quz7sKI8KXXbt33Al9vmirva9vO7Fjj0EqJ0I_ZXIPTI1h7Qn9n46V3ep6HI1lhc_a8byUfr2WuFu3hOXhiBFLlGaWKMEx6_6IQFPDCWw1IGTiqjvC0U_ICaml5waAJjcRO6Kl67dxrmFTHbEI13rIPHwisLL1bwO6_SUVebQeLrvK0KiP8KcRm4TurH5mkE0xGB2WPWeEFlFXlY5QQrcYkoq7b0L-UZAlGpOqiH_8gW6IzqLMLWFXeikUwe5qgAu--IVBafFlfDhueLYyumYeWH7PpTCWKJwZD_jqwSa-AnMmgoPsnEES_xkzXq0PlzlvifTeiiHE4ZsaeYYg7BbgSrR8z4mf6A9TtbNJu9Qg6A908lCGXQoLawM6cXffeKtJ7CY9z2HEoPVvm1em2nIqrgx1kZYomprhrigOY8FOmOCTSzG_SXL4ks4fnNDJcjJixv69IrjUvS18GZvQ1S_DRtxUqIEfCqfze627uLzk6ZNj2eJkKrEc1qTOZe_DX2wVy7rmbh4OUwUu3bUh8ImfkdO8zi0GQnS_A7HD52ZJcEQciTrbTFPrTihd5P8xH1XHQilmvMjfItZVNCwgZV0Z1DcloVF_cQQXmzsySIJfZWnc8VefqdLPYEJD8X-0E-HYIRKTRHXb9i09gWTqqhwNFuqlvT-vyk4soktwoXq8gVDPwCjAOLsMiW58Y_nV4KVKTvb4l0kJo=s1406-no?authuser=0' />
+         <img className='personal-img' src='https://firebasestorage.googleapis.com/v0/b/upload-image-96574.appspot.com/o/Personal%2FUntitled%20design%20(1).png?alt=media&token=0013842b-29a0-481b-9a6b-8cbe7acb44d3' />
          </div>
         </div>
 
       </div>
-
-
-
-
-
-
-
-    {/* <div className='contact-containerr'>
-
-    <div className='contact-container'>
-      
-      <h1 className='contact-header'>Get in touch</h1>
-
-      <p className='contact-subtext'>For any inquiries, or just to say hello, please contact me.</p>
-
-
-    <div className='icon-div'>
-
-      <a className='social-links' href="mailto:johnwilliamshellabarger@gmail.com"><FaGoogle className='social-icon' /><span>Gmail</span></a>
-      <a className='social-links' href="https://www.linkedin.com/in/johnshellabarger/" rel="noopener noreferrer" target="_blank"><FaLinkedinIn className='social-icon' /><span>LinkedIn</span></a>
-      <a className='social-links' href="https://github.com/johnshellabarger" rel="noopener noreferrer" target="_blank"><FaGithub className='social-icon' /><span>Github</span></a>
-    </div>
-
-      <button className='resume-btn'>VIEW RESUME</button>
-    
-    </div>
-    <div className='contact-img-container'>
-      <img className='contact-img'src='https://lh3.googleusercontent.com/KYye4SteAo-5Quz7sKI8KXXbt33Al9vmirva9vO7Fjj0EqJ0I_ZXIPTI1h7Qn9n46V3ep6HI1lhc_a8byUfr2WuFu3hOXhiBFLlGaWKMEx6_6IQFPDCWw1IGTiqjvC0U_ICaml5waAJjcRO6Kl67dxrmFTHbEI13rIPHwisLL1bwO6_SUVebQeLrvK0KiP8KcRm4TurH5mkE0xGB2WPWeEFlFXlY5QQrcYkoq7b0L-UZAlGpOqiH_8gW6IzqLMLWFXeikUwe5qgAu--IVBafFlfDhueLYyumYeWH7PpTCWKJwZD_jqwSa-AnMmgoPsnEES_xkzXq0PlzlvifTeiiHE4ZsaeYYg7BbgSrR8z4mf6A9TtbNJu9Qg6A908lCGXQoLawM6cXffeKtJ7CY9z2HEoPVvm1em2nIqrgx1kZYomprhrigOY8FOmOCTSzG_SXL4ks4fnNDJcjJixv69IrjUvS18GZvQ1S_DRtxUqIEfCqfze627uLzk6ZNj2eJkKrEc1qTOZe_DX2wVy7rmbh4OUwUu3bUh8ImfkdO8zi0GQnS_A7HD52ZJcEQciTrbTFPrTihd5P8xH1XHQilmvMjfItZVNCwgZV0Z1DcloVF_cQQXmzsySIJfZWnc8VefqdLPYEJD8X-0E-HYIRKTRHXb9i09gWTqqhwNFuqlvT-vyk4soktwoXq8gVDPwCjAOLsMiW58Y_nV4KVKTvb4l0kJo=s1406-no?authuser=0' />
-    </div>
-    </div> */}
     </div>
   )
 }
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,5 +1,7 @@
 import "./projects.css"
 import {  FaGithub } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+
 
 
 const Projects = () => {
@@ -8,13 +10,15 @@ const Projects = () => {
     <div className='page-container'>
 
 
-
       <div class="project-container">
         <div className='project-title'>
           <h1 className='title'>The Trading Post</h1>
           <a className='project-links' href="https://www.loom.com/share/d8e4391ee3aa469ebe30649088863e97" rel="noopener noreferrer" target="_blank"><span>VIEW DEMO</span></a>
-          <FaGithub className='github-icon'/>
+          <a className='github-link' href='https://github.com/johnshellabarger/trading-app' rel="noopener noreferrer" target="_blank">
+            <FaGithub className='github-icon'/>
+          </a>
         </div>
+        <img src="https://firebasestorage.googleapis.com/v0/b/upload-image-96574.appspot.com/o/Personal%2FScreen%20Shot%202021-09-29%20at%202.44.56%20PM.png?alt=media&token=8678794a-97d2-441c-919f-992c8f07fb1d" alt="Deja-Bru" className="image"/>
         <div class="overlay"></div>
       </div>
 
@@ -23,9 +27,11 @@ const Projects = () => {
         <div className='project-title'>
           <h1 className='title'>Deja-Bru</h1>
           <a className='project-links' href="https://www.loom.com/share/7be139fa08154adea08144e1b904bc4a" rel="noopener noreferrer" target="_blank"><span>VIEW DEMO</span></a>
-          <FaGithub className='github-icon'/>
+          <a className='github-link' href='https://github.com/johnshellabarger/deja-bru' rel="noopener noreferrer" target="_blank">
+            <FaGithub className='github-icon'/>
+          </a>
         </div>
-        <img src="https://lh3.googleusercontent.com/_zXbgkur-Mm0ksHgb1Lr-UkrcYQE0RC0466xWQLng5YjkDWPJPg8tNLS0Oj82TSWTWViWJfwttMIHFxKbg4JTD79YwNBl28ITjSWhN57FJH_c9GGAr6DPKZprBks63vAZHxINzDqFR4o3sVXGE7Vml7RJgUfPPEkQxniAN0e5ViG92hsZf1lWWFstSOW1tzU7IxCPkDPeOP0TeBLCTyLaHwgGCWzyi1z1GgYwrQk8FhZIC6Q1e7m9Tu05D5v3X50K2Zvedrz95gLeJ5PaPCcJZM0_aiDnsYlfLWQkS4GwImZlYRwBx2_N-3tv2VQSrBjxcRoBYECZ4H4AjtXz5qaN1UJVEegPn3FwpPKKn2vWUFRbKToI6LtGfJzEei6VmYwQyPBUz75ldJ1EWKevHCWBpcI7SPj57mna7tOa_ep5FxeSlWpLaa-aVDcmo590kSrU4mYYjCK1KQvN1i70Mqg_QNWRvJlwm4wU031nLoGyCKAU5u3y2PBf4JV5XFVa7fU3Dy9aId4ee8Vc5bS6vaEwiTw9MUFn38-T2lPldigZihkZ-65JBQ9rbcchbfQHVl3Pgp88q2QNh7x_3qAVDAOFvO3cqc9FRgNNjAju_azQ4cexdcc7gpihuiWMOHe4rGqRQDQISkTemwBb5fRCB4FHpuXg8GBVsd_jzVtdamgFuSH-sOr-RvvwhJBAILtbiEFLvWp8T4_sn8VWl-RRl9n7Jg=w2880-h1354-no?authuser=0" alt="Avatar" className="image"/>
+        <img src="https://firebasestorage.googleapis.com/v0/b/upload-image-96574.appspot.com/o/Personal%2FScreen%20Shot%202021-09-30%20at%2010.11.08%20AM.png?alt=media&token=27f5a4dc-892a-4af4-917c-0a787570f762" alt="Deja-Bru" className="image"/>
         <div class="overlay"></div>
       </div>
 
@@ -33,10 +39,11 @@ const Projects = () => {
         <div className='project-title'>
           <h1 className='title'>Trival</h1>
           <a className='project-links' href="https://www.loom.com/share/7ff326fcd94e436bbce235d042743e50" rel="noopener noreferrer" target="_blank"><span>VIEW DEMO</span></a>
-          <FaGithub className='github-icon'/>
-          
+          <a className='github-link' href='https://github.com/johnshellabarger/phase-1-project' rel="noopener noreferrer" target="_blank">
+            <FaGithub className='github-icon'/>
+          </a>
         </div>
-        <img src="https://lh3.googleusercontent.com/ShXZ9T842SlvHZh2xmrthainuVGOTTDyVE31XTBOihaWZJohSiZSOPJT0a7apv5ZJZFDp6E1hXU1kNZlHg0qpanV3ZQCTHRUT6d5YUP9e4LOXUiMCt_v0DChqd4sdinPjv77ELGuE59cYmPBA1InvLut_a3_07tuDUu4XKnHVcP9_3UWFzUO3UeMbF0olE5ZP_VccSHkhxNF6--RSpsueSDAPEE7xTwj8yjWT6VCAJHfg06GpFuTgo4nqj65TvL-eFs4gCMVXKTj65sxjSA5EkD6N81iUUcPKwcmQ5No9TcQ6JjZNdFl-T_BZbQBA3knGYe0CZaarKXhtJd21NcjUelhIf0lNr5Y9nQegydrEghCaSUJKjw1IWTxK-SK9a3JqcThIEhziC3Gi_k6ieqQdtpjGBAwcAAw3iiR3z39UO6r_thZQf2JQ93a6WCbtq459qVM8EPKh1-xbN8f4BhXYkQmCWZXBMTq-Njrx-hAUM6HfkkGxF0vqJloM2DPFC-BHaRJYREGc_GSdwyRhIShuWXGh9JjjgvdUWahVlLxtYm0vsi12wJO9D3LIwe_VuT1120_nLoCUebC4adoJZapGSQBsEyuO0oifO8Nr6Ab8s0QlDaycG_SKu6xstNFLUvod8C1q5sgz3e8dVoDpAvpUVdBLmF3_83NiBuHgcPu8_UgC2v3iFzQp4f-gpB1CzTxBe67xcvZV1a-CVxjzv2a3rM=w2880-h1354-no?authuser=0" alt="Avatar" className="image"/>
+        <img src="https://firebasestorage.googleapis.com/v0/b/upload-image-96574.appspot.com/o/Personal%2FScreen%20Shot%202021-09-29%20at%2010.05.27%20PM.png?alt=media&token=07afa72f-996d-4eb1-8f67-434a32d4ee4a" alt="Avatar" className="image"/>
         <div class="overlay"></div>
       </div>
 
